b2027f4b1e536700b99923766e5937163b3bea29
mickg <mickg@mickg.net>
2026-08-28 08:15:18 -0400

    S7: build simulator against product libraries

--- cache/sim/build_p50sim.sh
+++ cache/sim/build_p50sim.sh
@@ -3,13 +3,24 @@ set -eu
 
 sim_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 root=$(CDPATH= cd -- "$sim_dir/../.." && pwd)
-deps=/tanksmall/MICKG2/mickg/miniconda3/envs/cppdeps/include
-arrow=/tanksmall/MICKG2/mickg/miniconda3/envs/mta/lib/python3.11/site-packages/pyarrow/include
+dep_root=${ICECC_P50_DEP_ROOT:-/tanksmall/MICKG2/mickg/src/mickg10/icecream-deps/ubuntu22-p50-root/usr}
+cxx=${CXX:-g++}
+output="$sim_dir/.p50sim.bin"
+temporary=$(mktemp "$sim_dir/.p50sim.bin.XXXXXX")
+trap 'rm -f "$temporary"' EXIT HUP INT TERM
 
-exec g++ -std=c++23 -O2 -DBOOST_ERROR_CODE_HEADER_ONLY \
-    -I"$root" -I"$deps" -I"$arrow" -I"$arrow/arrow/vendored" \
-    "$sim_dir/p50sim.cpp" "$root/cache/protocol50.cpp" \
-    "$root/cache/p50_actions.cpp" "$root/cache/p50_zstd.cpp" \
-    "$root/cache/p50_endpoint.cpp" "$root/cache/p50_input_record.cpp" \
-    "$root/cache/p50_slice0.cpp" "$root/services/digest128.cpp" \
-    -lzstd -Wl,-l:libxxhash.so.0 -pthread -o "$sim_dir/.p50sim.bin"
+make -C "$root/cache" \
+    libprotocol50.a libp50endpoint.a libp50adoptedoutcomewriter.a
+make -C "$root/services" libicecc.la
+
+"$cxx" -std=c++23 -O2 -DBOOST_ERROR_CODE_HEADER_ONLY \
+    -I"$root" -I"$dep_root/include" \
+    "$sim_dir/p50sim.cpp" \
+    "$root/cache/libp50endpoint.a" \
+    "$root/cache/libp50adoptedoutcomewriter.a" \
+    "$root/cache/libprotocol50.a" \
+    "$root/services/.libs/libicecc.a" \
+    -L"$dep_root/lib/x86_64-linux-gnu" \
+    -lzstd -lxxhash -llzo2 -pthread -o "$temporary"
+mv "$temporary" "$output"
+trap - EXIT HUP INT TERM
